4610ee0f1cb4752a841b45d22765291b73f8efa6
Anton Alokhin <anton.alyokhin1997@gmail.com>
2023-06-04 23:16:07 +0300

    04.06.2023.23:16

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid'
 import ContactList from "./ContactList/ContactList";
 import Filter from "./Filter/Filter";
 import ContactForm from "./ContactForm/ContactForm";
 
-export const App =()=> {
- const [contacts, setContacts] = useState([
+const mountContacts = [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ]);
+    ];
+
+export const App =()=> {
+ const [contacts, setContacts] = useState(mountContacts);
   const [filter, setFilter] = useState('');
 
   const addContact = (task) => {
@@ -31,6 +33,10 @@ export const App =()=> {
       setContacts(prevState => [...prevState, contact]);
     }
   };
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const changeFilter = (filter) => {
     setFilter( filter );
@@ -61,12 +67,10 @@ export const App =()=> {
         <ContactForm onSubmit={addContact} />
         <h2>Contacts</h2>
           <Filter value={filter} onChangeFilter={changeFilter} />
-        {visibleContacts.length > 0 && (
-          <ContactList
+<ContactList
             contacts={visibleContacts}
             onRemoveContact={removeContact}
           />
-        )}
       </div>
     );
   }
